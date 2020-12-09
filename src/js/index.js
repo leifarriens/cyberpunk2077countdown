161a222ba803305477be8862b9b2f8fa5cb69d3d
Leif Arriens <30775450+leifarriens@users.noreply.github.com>
2020-12-09 06:10:27 +0100

    Mobile bg fix & analytics

--- src/js/index.js
+++ src/js/index.js
@@ -46,7 +46,9 @@ const bg = urlParams.get('bg');
 const bgRef = document.querySelector('.bg');
 
 if (/.webm|.mp4/.test(bg)) { // set video src
-  bgRef.querySelector('video').src = bg;
+  const video = bgRef.querySelector('video');
+  video.src = bg;
+  video.style.display = 'block';
 }
 
 if (/.jpg|.png|.gif/.test(bg)) {
